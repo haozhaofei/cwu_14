f79cadeeb078179c0c60f94078c1f6c0cd87fd58
lili <root_chaofei@126.com>
2017-05-04 12:45:07 +0800

    ......

---
+++ liaoxinting/js09/2/2.1.php
@@ -0,0 +1,4 @@
+<?php 
+$m1=$_REQUEST['m1'];
+$m2=$_REQUEST['m2'];
+echo $m1+$m2;
---
+++ liaoxinting/js09/4/4.3.php
@@ -0,0 +1,7 @@
+<?php 
+$name = $_GET['name'];
+if(in_array($name,array("ss","ss"))){
+	echo "yes";
+}else{
+	echo "no";
+}
